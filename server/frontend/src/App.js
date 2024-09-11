f2d14d645051e882689aee4041cea4584bc77f57
Laila <lailamohammadabdulaziz@gmail.com>
2024-09-11 18:13:02 +0300

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register.jsx"; // Import the Register component
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
 export default App;
+
