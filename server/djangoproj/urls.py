2df8d59b9d3457bce72a6b8c08e1c78459da72e1
Laila <lailamohammadabdulaziz@gmail.com>
2024-09-11 18:10:47 +0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,6 +1,6 @@
 """djangoproj URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
@@ -18,9 +18,14 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views  # Import your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+        path('about/', TemplateView.as_view(template_name="About.html")),
+            path('contact/', TemplateView.as_view(template_name="Contact.html")),
+                path('login/', TemplateView.as_view(template_name="index.html")),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
